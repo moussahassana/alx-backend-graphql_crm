2b069f81c19ae0a79b1276fdca2d2afe4af84ef7
Moussa Hassana <moussahassana42@gmail.com>
2025-06-28 22:19:23 +0100

    fix: change the project name to alx-backend-graphql

--- alx-backend-graphql/wsgi.py
+++ alx-backend-graphql/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for alx-backend-graphql_crm project.
+WSGI config for alx-backend-graphql project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx-backend-graphql_crm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx-backend-graphql.settings')
 
 application = get_wsgi_application()
